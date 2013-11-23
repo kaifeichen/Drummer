bb2b576f44537195f50022b5310c2f82190753c0
Beidi Chen <bettychen824@gmail.com>
2013-11-23 14:47:34 -0800

    add buttom

--- gen/com/example/firstapp/R.java
+++ gen/com/example/firstapp/R.java
@@ -24,15 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080005;
         public static final int frequency=0x7f080001;
         public static final int frequencytext=0x7f080000;
+        public static final int send_button=0x7f080004;
+        public static final int timemilisec=0x7f080003;
+        public static final int timetext=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_activityhaha=0x7f030000;
-        public static final int activity_display_message=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_send_voice=0x7f030003;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_send_voice=0x7f030001;
     }
     public static final class menu {
         public static final int activityhaha=0x7f070000;
@@ -43,13 +44,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050002;
         public static final int edit_message=0x7f050001;
         public static final int hello_world=0x7f050005;
-        public static final int title_activity_activityhaha=0x7f050008;
+        public static final int send_button=0x7f050002;
         public static final int title_activity_display_message=0x7f050006;
         public static final int title_activity_main=0x7f050004;
-        public static final int title_activity_send_voice=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/example/firstapp/MainActivity.java
+++ src/com/example/firstapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.firstapp;
+
 import com.example.firstapp.R;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -13,9 +18,12 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     Thread t;
     int sr = 44100;
+    long S0;
     boolean isRunning = true;
     SeekBar fSlider;
     TextView Viewer;
+    Button Send;
+    EditText Sendtime;
     double sliderval;
 
     @Override
@@ -24,49 +32,64 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         fSlider = (SeekBar) findViewById(R.id.frequency);
         Viewer = (TextView) findViewById(R.id.frequencytext);
+        Send = (Button) findViewById(R.id.send_button);
+        Sendtime = (EditText) findViewById(R.id.timemilisec);      
+        
         OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
             public void onStopTrackingTouch(SeekBar seekBar) { }
             public void onStartTrackingTouch(SeekBar seekBar) { }
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if(fromUser) sliderval = progress / (double)seekBar.getMax();
                 Viewer.setText(Double.toString(2000 + 20000*sliderval));
-
+                
             }
         };
-
+        
         fSlider.setOnSeekBarChangeListener(listener);
+        
+        Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	t = new Thread() {
+                	@SuppressLint("NewApi") @Override     	  
+                    public void run() {
 
-        t = new Thread() {
-        	@Override     	  
-            public void run() {
+                        setPriority(Thread.MAX_PRIORITY);
 
-                setPriority(Thread.MAX_PRIORITY);
+                        int buffsize = AudioTrack.getMinBufferSize(sr, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
 
-                int buffsize = AudioTrack.getMinBufferSize(sr, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sr, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, buffsize, AudioTrack.MODE_STREAM);
+                        long S0;
+                        if (Sendtime.getText().toString().isEmpty()){
+                        	S0=0;
+                        }
+                        else{
+                        	S0= Long.parseLong(Sendtime.getText().toString());
+                        }
+                        
+                        short samples[] = new short[buffsize];
+                        int amp = 10000;
+                        double twopi = 8.*Math.atan(1.);
+                        double fr = 2000.f;
+                        double ph = 0.0;
 
-                AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sr, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, buffsize, AudioTrack.MODE_STREAM);
-
-                short samples[] = new short[buffsize];
-                int amp = 10000;
-                double twopi = 8.*Math.atan(1.);
-                double fr = 2000.f;
-                double ph = 0.0;
-
-                audioTrack.play();
-
-                while(isRunning){
-                    fr =  2000 + 20000*sliderval;
-                    for(int i=0; i < buffsize; i++){
-                        samples[i] = (short) (amp*Math.sin(ph));
-                        ph += twopi*fr/sr;
+                        audioTrack.play();
+                        long startTime=System.currentTimeMillis();
+                        while(System.currentTimeMillis()-startTime<S0){
+                            fr =  2000 + 20000*sliderval;
+                            for(int i=0; i < buffsize; i++){
+                                samples[i] = (short) (amp*Math.sin(ph));
+                                ph += twopi*fr/sr;
+                            }
+                            audioTrack.write(samples, 0, buffsize);
+                        }
+                        audioTrack.stop();
+                        audioTrack.release();
                     }
-                    audioTrack.write(samples, 0, buffsize);
-                }
-                audioTrack.stop();
-                audioTrack.release();
-            }
-        };
-        t.start();        
+                };
+                t.start();        
+            }          
+        });
     }
 
     @Override
