a09bedfb87d4420cb28160688686da772dd9fbff
Kaifei Chen <kaifei@cs.berkeley.edu>
2014-02-10 11:54:56 -0800

    Fix the bug that AudioRecorder cannot be initialized
    - Fix the bug that AudioRecorder cannot be initialized
    - Add proper permissions in AndroidManifest.cml
    - Put Send and Recv Thread in separate definition
    - Schedule task to stop recording

--- drummer/src/edu/berkeley/drummer/MainActivity.java
+++ drummer/src/edu/berkeley/drummer/MainActivity.java
@@ -1,11 +1,12 @@
 package edu.berkeley.drummer;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.json.JSONArray;
-
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -16,6 +17,7 @@ import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -25,214 +27,227 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    Thread t;
-    Thread s;
-    int sr = 44100;
-    long S0;
-    boolean isRunning = true;
-    SeekBar fSlider;
-    TextView Viewer;
-    Button Send;
-    EditText Sendtime;
-    double sliderval;
-    double maxDuration = 50;
-    
-    @Override
-    public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        fSlider = (SeekBar) findViewById(R.id.frequency);
-        Viewer = (TextView) findViewById(R.id.frequencytext);
-        Send = (Button) findViewById(R.id.send_button);
-        Sendtime = (EditText) findViewById(R.id.timemilisec);
+	private AudioSendThread t;
+	private AudioRecordThread s;
+	int sr = 44100;
+	long S0;
+	boolean isRunning = true;
+	SeekBar fSlider;
+	TextView Viewer;
+	Button Send;
+	EditText Sendtime;
+	double sliderval;
+	double maxDuration = 50;
+	private Handler mHandler;
 
-        final OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(final SeekBar seekBar) {
-            }
+	private class AudioSendThread extends Thread {
+		@Override
+		public void run() {
 
-            @Override
-            public void onStartTrackingTouch(final SeekBar seekBar) {
-            }
+			setPriority(Thread.MAX_PRIORITY);
 
-            @Override
-            public void onProgressChanged(final SeekBar seekBar,
-                    final int progress, final boolean fromUser) {
-                if (fromUser)
-                    sliderval = progress / (double) seekBar.getMax();
-                Viewer.setText(Double.toString(0 + 22050 * sliderval));
+			final int buffsize = AudioTrack.getMinBufferSize(sr,
+					AudioFormat.CHANNEL_OUT_MONO,
+					AudioFormat.ENCODING_PCM_16BIT);
+			final AudioTrack audioTrack = new AudioTrack(
+					AudioManager.STREAM_ALARM, sr,
+					AudioFormat.CHANNEL_OUT_MONO,
+					AudioFormat.ENCODING_PCM_16BIT, buffsize,
+					AudioTrack.MODE_STREAM);
+			double S0 = 0;
+			if (Sendtime.getText() != null) {
+				if (Sendtime.getText().toString().length() > 0) {
+					S0 = Double.parseDouble(Sendtime.getText().toString()) / 1000.0;
+				}
+			}
 
-            }
-        };
+			final short amp = 32000;
+			final double twopi = 8. * Math.atan(1.);
+			double ph = 0.0;
+			final double fr = 0 + 22050 * sliderval;
+			final double count = S0 * sr;
+			int sample = 1;
+			int remain = 0;
+			if (count < buffsize) {
+				remain = (int) count;
+			} else {
+				sample = (int) (count / buffsize);
+				remain = (int) (count - sample * buffsize);
+			}
+			final short samples[] = new short[buffsize * sample];
 
-        fSlider.setOnSeekBarChangeListener(listener);
+			for (int j = 0; j < sample; j++) {
+				for (int i = 0; i < buffsize; i++) {
+					if ((j == sample - 1) && i > remain - 1) {
+						samples[j * buffsize + i] = (short) 0;
+					} else {
+						samples[j * buffsize + i] = (short) (amp * Math.sin(ph));
+					}
+					ph += twopi * fr / sr;
+				}
+			}
+			audioTrack.play();
+			audioTrack.write(samples, 0, buffsize * sample);
+			audioTrack.stop();
+			audioTrack.release();
+		}
+	}
 
-        Send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                t = new Thread() {
-                    @SuppressLint("NewApi")
-                    @Override
-                    public void run() {
+	private class AudioRecordThread extends Thread {
+		private volatile boolean mRun = true;
+		private AudioRecord mRecorder;
+		private BufferedOutputStream os = null;
 
-                        setPriority(Thread.MAX_PRIORITY);
+		@Override
+		public void run() {
+			final File sdCard = Environment.getExternalStorageDirectory();
+			final File directory = new File(sdCard.getAbsolutePath()
+					+ "/Drummer");
+			final String fileName = Long.toString(System.currentTimeMillis())
+					+ ".pcm";
+			final File file = new File(directory, fileName);
+			try {
+				os = new BufferedOutputStream(new FileOutputStream(file));
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				final int bufferSize = AudioRecord.getMinBufferSize(44100,
+						AudioFormat.CHANNEL_IN_MONO,
+						AudioFormat.ENCODING_PCM_16BIT);
+				System.out.println(bufferSize);
+				mRecorder = new AudioRecord(
+						MediaRecorder.AudioSource.CAMCORDER, 44100,
+						AudioFormat.CHANNEL_IN_MONO,
+						AudioFormat.ENCODING_PCM_16BIT, bufferSize);
 
-                        final int buffsize = AudioTrack.getMinBufferSize(sr,
-                                AudioFormat.CHANNEL_OUT_MONO,
-                                AudioFormat.ENCODING_PCM_16BIT);
-                        final AudioTrack audioTrack = new AudioTrack(
-                                AudioManager.STREAM_ALARM, sr,
-                                AudioFormat.CHANNEL_OUT_MONO,
-                                AudioFormat.ENCODING_PCM_16BIT, buffsize,
-                                AudioTrack.MODE_STREAM);
-                        double S0 = 0;
-                        if (Sendtime.getText() != null) {
-                            if (!Sendtime.getText().toString().isEmpty()) {
-                                S0 = Double.parseDouble(Sendtime.getText()
-                                        .toString()) / 1000.0;
-                            }
-                        }
+				mRecorder.startRecording();
 
-                        final short amp = 32000;
-                        final double twopi = 8. * Math.atan(1.);
-                        double ph = 0.0;
-                        final double fr = 0 + 22050 * sliderval;
-                        final double count = S0 * sr;
-                        int sample = 1;
-                        int remain = 0;
-                        if (count < buffsize) {
-                            remain = (int) count;
-                        } else {
-                            sample = (int) (count / buffsize);
-                            remain = (int) (count - sample * buffsize);
-                        }
-                        final short samples[] = new short[buffsize * sample];
+				while (mRun == true) {
+					final byte[] buffer = new byte[bufferSize];
+					// blocking read, which returns when
+					// buffer.length bytes are recorded
+					mRecorder.read(buffer, 0, buffer.length); // Bytes
 
-                        for (int j = 0; j < sample; j++) {
-                            for (int i = 0; i < buffsize; i++) {
-                                if ((j == sample - 1) && i > remain - 1) {
-                                    samples[j * buffsize + i] = (short) 0;
-                                } else {
-                                    samples[j * buffsize + i] = (short) (amp * Math
-                                            .sin(ph));
-                                }
-                                ph += twopi * fr / sr;
-                            }
-                        }
-                        audioTrack.play();
-                        audioTrack.write(samples, 0, buffsize * sample);
-                        audioTrack.stop();
-                        audioTrack.release();
-                    }
-                };
-                
-                s = new Thread(){
+					os.write(buffer);
+				}
+				os.flush();
+				os.close();
+				mRecorder.stop();
+				mRecorder.release();
 
-                    private volatile boolean mRun = true;
-                    private Long mStartEpoch;
-                    private AudioRecord mRecorder;
-                    private final JSONArray mRaw = new JSONArray();
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		}
 
-                    @Override
-                    public void run() {
-                      String filepath = Environment.getExternalStorageDirectory().getPath();
-//                    	String filepath = "/sdcard";
-                      FileOutputStream os = null;
-                      try {
-						os = new FileOutputStream(filepath+"/record.pcm");
-                      } catch (FileNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-                      }
-                      try {
-                        int mChannel;
-//        				final int bufferSize = AudioRecord.getMinBufferSize(44100,
-//                            16, 2);
-                        final int bufferSize =  AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_STEREO,
-                                AudioFormat.ENCODING_PCM_16BIT);
-        				System.out.println(bufferSize);
-        			
-//                        mRecorder = new AudioRecord(2, 44100, 16, 2,
-//                            bufferSize);
-        				mRecorder = new AudioRecord(MediaRecorder.AudioSource.CAMCORDER, 44100, AudioFormat.CHANNEL_IN_STEREO,
-                                AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+		public void terminate() {
+			mRun = false;
+		}
+	}
 
-                        mStartEpoch = System.currentTimeMillis();
-                        mRecorder.startRecording();
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		fSlider = (SeekBar) findViewById(R.id.frequency);
+		Viewer = (TextView) findViewById(R.id.frequencytext);
+		Send = (Button) findViewById(R.id.send_button);
+		Sendtime = (EditText) findViewById(R.id.timemilisec);
+		mHandler = new Handler();
 
-                        while (!this.isInterrupted()) {
-                          final byte[] buffer = new byte[bufferSize];
-                          // blocking read, which returns when buffer.length bytes are recorded
-                          mRecorder.read(buffer, 0, buffer.length); // Bytes
-//                          for (final byte data : buffer) {
-//                            mRaw.put(data);
-//                            //save filec
-//                          }
-                          try {
-                              os.write(buffer);
-                          } catch (IOException e) {
-                              e.printStackTrace();
-                          }
+		final OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
+			@Override
+			public void onStopTrackingTouch(final SeekBar seekBar) {
+			}
 
-                        }
-                        try {
-                            os.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        mRecorder.stop();
-                        mRecorder.release();
+			@Override
+			public void onStartTrackingTouch(final SeekBar seekBar) {
+			}
 
-                      } catch (final Exception e) {
-                        e.printStackTrace();
-                      }
-                    }
+			@Override
+			public void onProgressChanged(final SeekBar seekBar,
+					final int progress, final boolean fromUser) {
+				if (fromUser)
+					sliderval = progress / (double) seekBar.getMax();
+				Viewer.setText(Double.toString(0 + 22050 * sliderval));
 
-                    public void terminate() {
-                      mRun = false;
-                    }
-                  };
+			}
+		};
 
-                double duration = Double.parseDouble(Sendtime.getText()
-                        .toString());
+		fSlider.setOnSeekBarChangeListener(listener);
 
-                if (duration > maxDuration) {
-                    duration = maxDuration;
-                    Sendtime.setText(Double.toString(duration));
-                }
-                         
-                s.start();
-                t.start();
-//                try {
-//					t.join();
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-                
-                
-                
-            }
-        });
-        
-        
-    }
+		Send.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				Send.setEnabled(false);
+				t = new AudioSendThread();
+				s = new AudioRecordThread();
 
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+				double sendDuration = Double.parseDouble(Sendtime.getText()
+						.toString());
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        isRunning = false;
-        s.interrupt();
-        try {
-            t.join();
-        } catch (final InterruptedException e) {
-            e.printStackTrace();
-        }
-        t = null;
-    }
+				if (sendDuration > maxDuration) {
+					sendDuration = maxDuration;
+					Sendtime.setText(Double.toString(sendDuration));
+				}
+
+				long recvDuration = 1000;
+
+				// This will get the SD Card directory and create a folder named
+				// MyFiles in it.
+				final File sdCard = Environment.getExternalStorageDirectory();
+				final File directory = new File(sdCard.getAbsolutePath()
+						+ "/Drummer");
+				directory.mkdirs();
+
+				
+				t.start();
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				s.start();
+				mHandler.postDelayed(mPauseTask, recvDuration);
+			}
+		});
+
+	}
+
+	private final Runnable mPauseTask = new Runnable() {
+		@Override
+		public void run() {
+			s.terminate();
+			try {
+				s.join();
+			} catch (final InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			mHandler.removeCallbacks(mPauseTask);
+			Send.setEnabled(true);
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		isRunning = false;
+		s.interrupt();
+		try {
+			t.join();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+		t = null;
+	}
 }
