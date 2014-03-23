827117d93bb5ff8db01c8ff76752523c5db16075
Beidi Chen <bettychen824@gmail.com>
2014-03-22 23:38:09 -0700

    combine audioreduction and calculate distance

--- analysis/audio_reduction.py
+++ analysis/audio_reduction.py
@@ -5,129 +5,55 @@ import scipy as sp
 import struct
 import matplotlib.pyplot as plt
 from scipy import optimize
+TIME = 1.0 / 44100
 
-TIME = 1.0/44100
 
-def reduce(data, origin_func):
+def reduction(data, startf=5000.0, endf=5100.0, length=3.0):
     results = []
-    # data = [0,1,2,3,4,5]
+    counter = 0
 
-    # get index of first value in data that is larger than 10000
-    # TODO this value (15000) needs to be changed when freq is not around 5K Hz
-    offset = (i for i,v in enumerate(data) if v > 15000).next()
-    results = data[offset:offset+int(origin_func.duration/TIME)]
-    # print amp
-    value = 1000000000000000000
+    for elem in data:
+        if elem > 10000:
+            for i in range(int(length / TIME)):
+                results.append(data[counter + i])
+            break
+        counter += 1
+    amp = max(results[:30])
+    value = float("inf")
     starttime = 0
-    # TODO this value (160) needs to be changed when freq is not around 5K Hz
-    for start in np.arange(0, 2*TIME, TIME/80):
-        # origin = [function(start, amp, startf, endf, length) for i in range(len(results))]
-        # origin = [function(start) for  in range(len(results))]
+    for j in range(640):
+        start = j * TIME / 80.0
         origin = []
         error = 0
-        for i in range(10):
-            # origin.append(function(start+i*TIME))
-            error+=abs(results[i]-origin_func(start+i*TIME))
-        # local_max = max(np.correlate(origin, results, "full"))
-        # error = [(results[i]-origin[i]) for i in range(len(results))]
-        #print error
-        if (error<value):
+        for i in range(30):
+            error += abs(results[i] -
+                         chirp_func(start + i * TIME, amp, startf, endf, length))
+        if (error < value):
             starttime = start
             value = error
-        #print "got here", j
 
-    #f = open(RESULT, 'w')
     num = 0
     plot1 = []
-    kaifei_now=[]
-    kaifei_past=[]
-    # print len(data)
-    # print len(results)
-    while (num<len(data)):
-        if (num>=offset and num < offset+len(results)):
+    now_array = []
+    past_array = []
+    while (num < len(data)):
+        if (num >= counter and num < counter + len(results)):
             now = data[num]
-            past = origin_func(starttime + TIME*(num-offset))
-            #f.write(str(now-past)+' ')
-            plot1.append(now-past)
-            kaifei_past.append(past)
-            kaifei_now.append(now)
-        
+            past = chirp_func(starttime + TIME * (num - counter),
+                            amp, startf, endf, length)
+            plot1.append(now - past)
+            past_array.append(past)
+            now_array.append(now)
         else:
-            #f.write(str(data[num])+' ')
             plot1.append(data[num])
-        num+=1
-    # plt.plot(plot1)
-    # plt.show()
-
-    print "a =", kaifei_now
-    print "b =", kaifei_past
-    return np.array(kaifei_now) - np.array(kaifei_past)
-
-    # with open(filename, 'r') as inputfile:
-    #     linenumber = 0
-    #     for row in csv.reader(inputfile):
-    #         linenumber+=1
-    #         if linenumber>5: 
-    #             if (row[0]!="[-inf]" and row[0]!="[inf]"):   
-    #                 results.append(row[0])
-
-    # for elem in results:
-
-    #     sample = float(elem)
-    #     ratio = 10**(sample/10.0)
-    #     if (ratio>0.9):
-    #         point2 = 10**(float(results[counter])/10.0)
-    #         point3 = 10**(float(results[counter+1])/10.0)
-
-    #         amp = solve(func, point2, point3)
-    #         determinetime = 0
-    #         errortime = 1
-    #         for i in range(20):
-    #             error = abs(function((time+i*(time/20.0)), amp, startf, endf, length)-ratio)
-    #             if (error<errortime):
-    #                 errortime = error
-    #                 determinetime = time+i*(time/20.0)
-    #         break
-    # counter+=1
-
-    # for j in range((int)(length/1000.0*time)):
-    #     origin = 10**(float(results[counter-1+j])/10.0)
-    #     reduction = abs(function(determinetime+(j-1)*time))
-    #     results[counter-1+j] = 10.0*math.log10(abs(origin-reduction))
-
-    # with open(RESULT, 'w') as resultfile:
-    #     for elem in results:
-    #         resultfile.write(elem)
+        num += 1
+    return np.array(now_array) - np.array(past_array)
 
 
-# def solve(func, point1, point2):
-#     print (point1, point2)
-#     a= abs(sp.optimize.newton(func, -800, None, (point1, point2)))
-#     return a
-
-
-
-# def function(t):
-#     return t
-
-
-def func(a, point1, point2):
-    #TODO
-    # function()
-    # point1=asin(t)
-    # point2=asin(t+time)
-    # t=arcsin(point1/a)
-    # t+time=arcsin(point2/a)
-    return np.arcsin(point1/a)-np.arcsin(point2/a)-time
-    
-
-def read(filename):
-    f = open(filename, 'rb').read()
-    data_tuple = struct.unpack('<'+'h'*(len(f)/2), bytearray(f))
-    return data_tuple
-
-def main():
-    reduction(FILE)
-
-if __name__ == "__main__":
-    main()
+def chirp_func(t, a, start_fr, end_fr, length):
+    k = math.pow(float(end_fr) / start_fr, 1.0 / length)
+    den = math.log(k)
+    num = math.pow(k, t) - 1
+    rest = math.pi * 2 * start_fr
+    result = math.sin(1.0 * num * rest / den)
+    return a * result
--- analysis/distance.py
+++ analysis/distance.py
@@ -1,12 +1,13 @@
 import struct
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy import signal
 import sys
 import bpfilter
-import audio_reduction as reduction
 import math
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import audio_reduction
 
+from scipy import signal
 
 
 def find_echo(fname, duration):
@@ -17,7 +18,7 @@ def find_echo(fname, duration):
     f = open(fname, 'rb')
     data = f.read()
 
-    amp = struct.unpack("<"+"h"*(len(data)/2), data)
+    amp = struct.unpack("<" + "h" * (len(data) / 2), data)
     hilbert = np.abs(signal.hilbert(amp))
     result = [1 if x > 6700 else 0 for x in hilbert]
 
@@ -31,36 +32,54 @@ def plot(fname):
     plt.plot(hilbert, 'r', alpha=0.3)
     plt.show()
 
+def find_distance(reduced_amp, ratio=0.1):
+    length = len(reduced_amp)
+    threshold = max(reduced_amp) * ratio
+    counter=0
+    for i in range(length):
+        if (abs(reduced_amp[i]) > threshold):
+            counter += 1
+        if counter > 6:
+            return i - 4
 
-def chirp_func(t, a, start_fr, end_fr, length):
-    k = math.pow(float(end_fr)/start_fr, 1.0/length)
-    den = math.log(k)
-    num = math.pow(k,t)-1
-    rest = math.pi*2*start_fr
-    result = math.sin(1.0*num*rest/den)
-    return a*result
+if __name__ == "__main__":
 
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--fname', help="input file")
+    ap.add_argument('-p', '--png', help="output")
+    ap.add_argument('-s', '--startfreq', type=int, help="low frequency")
+    ap.add_argument('-e', '--endfreq', type=int, help="high frequency")
+    ap.add_argument('-d', '--duration', type=float, help="duration")
 
-if __name__ ==  "__main__":
-    fname = sys.argv[1]
-    lowfreq = int(sys.argv[2])
-    highfreq = int(sys.argv[3])
-    duration = float(sys.argv[4]) # in second
+    args = ap.parse_args()
 
-    f = open(fname, 'rb')
+    lowfreq = args.startfreq
+    highfreq = args.endfreq
+    duration = args.duration
+
+    f = open(args.fname, 'rb')
     data = f.read()
-    amp = np.array(struct.unpack("<"+"h"*(len(data)/2), data))
+    amp = np.array(struct.unpack("<" + "h" * (len(data) / 2), data))
+
     lowcut = lowfreq - 1000
     highcut = highfreq + 1000
     fs = 44100
 
-    filtered_amp = bpfilter.butter_bandpass_filter(amp, lowcut, highcut, fs, order=6)
-    origin_chirp_func = lambda t: chirp_func(t, max(abs(filtered_amp)), lowfreq, highfreq, duration)
-    origin_chirp_func.duration = duration
-    reduced_amp = reduction.reduce(filtered_amp, origin_chirp_func)
+    filtered_amp = bpfilter.butter_bandpass_filter(
+        amp, lowcut, highcut, fs, order=6)
 
-   # plt.plot(filtered_amp, 'b', alpha=0.2)
+
+    plt.plot(filtered_amp, 'r', alpha=0.3)
+    
+
+    reduced_amp = audio_reduction.reduction(
+        filtered_amp, lowfreq, highfreq, duration)
+
     plt.plot(reduced_amp, 'r', alpha=0.3)
-    # print len(filtered_amp)
-    # print len(reduced_amp)
     plt.show()
+
+    time = float(find_distance(reduced_amp)) / 44100 * 340 / 2 
+    print time
+
+    # plt.plot(reduced_amp, 'r', alpha=0.3)
+    # plt.savefig(args.png)
