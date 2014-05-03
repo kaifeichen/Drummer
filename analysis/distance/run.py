861834e5068942526ec967afea6afab1efb18350
Kaifei Chen <kaifei@cs.berkeley.edu>
2014-05-03 15:51:04 -0700

    add peak detect in distance measurement

--- analysis/distance/run.py
+++ analysis/distance/run.py
@@ -6,7 +6,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-
+import peakdetect
 
 
 if __name__ == "__main__":
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     ap.add_argument('-p', '--png', help="output")
     ap.add_argument('-s', '--startfreq', type=int, help="low frequency")
     ap.add_argument('-e', '--endfreq', type=int, help="high frequency")
-    ap.add_argument('-d', '--duration', type=float, help="duration")
+    #ap.add_argument('-d', '--duration', type=float, help="duration")
 
     args = ap.parse_args()
 
     lowfreq = args.startfreq
     highfreq = args.endfreq
-    duration = args.duration
+    #duration = args.duration
 
     f = open(args.fname, 'rb')
     data = f.read()
@@ -38,5 +38,14 @@ if __name__ == "__main__":
 
     plt.plot(filtered_amp, 'b', alpha=0.3)
     envelop_amp = abs(signal.hilbert(filtered_amp))
+
+    maxtab, _ = peakdetect.peakdet(envelop_amps, delta=2e8)
+    maxidx = numpy.argmax(maxtab[:,1])
+    maxtab = maxtab[maxidx:,:]
+    print maxtab
+    distances = (maxtab[:,0][1:] - maxtab[:,0][0]) / 44100 * 340 /2
+    print distances
+    plt.scatter(array(maxtab)[:,0], array(maxtab)[:,1], color='red')
+
     plt.plot(envelop_amp, 'r', alpha=0.3)
     plt.show()
